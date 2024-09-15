35a921b726dc97f708fb139c9fc6873b175c0927
caglarinhesap <caglarkantas06@hotmail.com>
2024-09-16 02:41:18 +0300

    -Move development.
    -Path block detection added.

--- Assets/Scripts/Interfaces/ISoldier.cs
+++ Assets/Scripts/Interfaces/ISoldier.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public interface ISoldier : IUnit
 { 
     public int Damage { get; set; }
+    public Vector2 CurrentPosition { get; set; }
+    public Vector2 TargetPosition { get; set; }
     public List<PathNode> path { get; set; }
     public void SetPath(Vector2 currentLocation, Vector2 targetLocation);
-    public void Move()
+    public void Move();
     public void Attack();
 }
--- Assets/Scripts/Models/BaseSoldier.cs
+++ Assets/Scripts/Models/BaseSoldier.cs
@@ -6,6 +6,8 @@ public abstract class BaseSoldier : MonoBehaviour, ISoldier
 {
     public int Health { get; set; }
     public int Damage { get; set; }
+    public Vector2 CurrentPosition { get; set; }
+    public Vector2 TargetPosition { get; set; }
     public List<PathNode> path { get; set; }
     public List<Vector2> OccupiedSquares { get; set; }
 
@@ -18,6 +20,9 @@ public abstract class BaseSoldier : MonoBehaviour, ISoldier
 
     public virtual void SetPath(Vector2 currentLocation, Vector2 targetLocation)
     {
+        CurrentPosition = currentLocation;
+        TargetPosition = targetLocation;
+
         path = GameManager.Instance.mapController.SetTargetPosition((int)currentLocation.x, (int)currentLocation.y, (int)targetLocation.x, (int)targetLocation.y);
 
         if (path != null && path.Count > 0)
@@ -32,13 +37,24 @@ public abstract class BaseSoldier : MonoBehaviour, ISoldier
         {
             Vector3 worldPosition = GameManager.Instance.mapController.mapView.GetWorldPositionFromGrid(new Vector2(path[0].x, path[0].y));
 
+            transform.DOKill();
             transform.DOMove(worldPosition, 0.5f).SetEase(Ease.Linear).OnComplete(() => {
                 GameManager.Instance.mapController.mapModel.GetPathfinding().GetNode((int)OccupiedSquares[0].x, (int)OccupiedSquares[0].y).SetIsWalkable(true);
                 OccupiedSquares.RemoveAt(0);
                 OccupiedSquares.Add(new Vector2(path[0].x, path[0].y));
+
+                if (path.Count > 1)
+                {
+                    CurrentPosition = new Vector2(path[1].x, path[1].y);
+                }
+
                 GameManager.Instance.mapController.mapModel.GetPathfinding().GetNode(path[0].x, path[0].y).SetIsWalkable(false);
                 path.RemoveAt(0);
-                Move();
+
+                if (path.Count > 0)
+                {
+                    SetPath(CurrentPosition, TargetPosition); //Set path again after one move. If path is blocked it will find new path.
+                }
             });
         }
     }
